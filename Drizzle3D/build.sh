72ae674f6bd7b73dc02f663a7c67271803543724
CherryTree56567 <124994670+Cherrytree56567@users.noreply.github.com>
2024-03-05 18:29:01 +1100

    G

--- Drizzle3D/build.sh
+++ Drizzle3D/build.sh
@@ -1,5 +1,4 @@
 sudo apt install -y git gcc
 git submodule init
 git submodule update
-gcc -Ivendor -Ivendor/Glad/include -Ivendor/GLFW/include -Ivendor/imgui -Ivendor/glm -Ivendor/Assimp/include -DDrizzle3D_LINUX -fPIC -std=c++20 -fpermissive -c *.cpp
-ar rcs x64/Debug/libDrizzle3D.a *.o
+gcc -Ivendor -Ivendor/Glad/include -Ivendor/GLFW/include -Ivendor/imgui -Ivendor/glm -Ivendor/Assimp/include -DDrizzle3D_LINUX -fPIC -std=c++20 -fpermissive  -shared -o x64/Debug/libDrizzle3D.so *.cpp
